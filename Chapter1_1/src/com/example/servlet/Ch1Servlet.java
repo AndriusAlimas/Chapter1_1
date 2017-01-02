3967e00fa5814b7fa9e9f5e494208c6a8654eec1
Andrius <andriusjavait@gmail.com>
2017-01-02 20:27:02 +0000

    final update for chapter 1 servlet code

--- Chapter1_1/src/com/example/servlet/Ch1Servlet.java
+++ Chapter1_1/src/com/example/servlet/Ch1Servlet.java
@@ -1,6 +1,22 @@
 package com.example.servlet;
 import javax.servlet.*;
 import javax.servlet.http.*;
-public class Ch1Servlet extends HttpServlet{
+import java.io.*;
 
+public class Ch1Servlet extends HttpServlet{
+	public void doGet(HttpServletRequest request, 
+			HttpServletResponse response)throws ServletException,IOException{
+		// ServletRequest has method getWriter() to get PrintWriter object
+		PrintWriter out = response.getWriter();
+		// simply create current Date object:
+		java.util.Date today = new java.util.Date();
+		// using PrintWriter earlier created object we use to create html page
+		// Its not recommended do this way, but for purpose of this training:
+		
+		out.println("<html> "
+				+ "<body> "
+				+ "<h1 align=center>Head First Servlet & JSP Chapter 1_1</h1><br>"
+				+ "today is : <b>"+ today + "</b></body>"
+				+ "</html>");
+	}
 }
